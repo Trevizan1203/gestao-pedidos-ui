68170ad8f132160ec3e75cc9ac25757d13bd862f
Gustavo Trevizan <gustavo-trevizan@hotmail.com>
2025-11-14 13:15:23 -0300

    application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { GestaoPedidosComponent } from './pages/gestao-pedidos/gestao-pedidos.component';
+import { ProductListComponent } from './component/product-list/product-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/pedidos', pathMatch: 'full' },
+  { path: 'pedidos', component: GestaoPedidosComponent },
+  { path: 'produtos', component: ProductListComponent },
+  { path: 'dashboard', component: GestaoPedidosComponent },
+  { path: 'clientes', component: GestaoPedidosComponent },
+  { path: 'relatorios', component: GestaoPedidosComponent }
+];
